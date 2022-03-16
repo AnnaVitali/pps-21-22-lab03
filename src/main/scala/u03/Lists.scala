6cdb444f8d1df7679f394634288cd56e32b42bc3
Anna <anna.vitali4@studio.unibo.it>
2022-03-16 18:20:42 +0100

    finish es list

--- src/main/scala/u03/Lists.scala
+++ src/main/scala/u03/Lists.scala
@@ -1,5 +1,8 @@
 package u03
 
+import u02.Modules.{Person, isStudent}
+import u02.Optionals.Option
+
 object Lists extends App:
 
   // A generic linkedlist
@@ -26,7 +29,11 @@ object Lists extends App:
       case Cons(_, t) => filter(t)(pred)
       case Nil() => Nil()*/
 
-    def filter[A](l1: List[A])(pred: A => Boolean): List[A] = ???
+    def filter[A](l1: List[A])(pred: A => Boolean): List[A] =
+      flatMap(l1)(v => pred(v) match
+        case true => Cons(v, Nil())
+        case false => Nil()
+      )
 
     def drop[A](l: List[A], n: Int): List[A] = l match
       case Cons(h, t) => n match
@@ -41,6 +48,38 @@ object Lists extends App:
     def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] = l match
       case Cons(h, t) => append(f(h), flatMap(t)(f))
       case Nil() => Nil()
+
+    import u02.Optionals
+
+    def max(l: List[Int]): Option[Int] =
+      def _max(l: List[Int], currentMax: Option[Int]): Option[Int] = l match
+        case Cons(h,t) =>
+          currentMax match
+            case Option.Some(v) if h >= v  => _max(t, Option.Some(h))
+            case Option.None() => _max(t, Option.Some(h))
+            case _ => _max(t, currentMax)
+        case Nil() => currentMax
+      _max(l, Option.None())
+
+    def getTeacherCourses(l: List[Person]): List[String] =
+      flatMap(l)(v => v match
+        case Person.Teacher(_, c) => Cons(c, Nil())
+        case _ => Nil()
+      )
+
+    def foldLeft[A,B](l: List[A])(acc: B)(f: (B, A) => B): B = l match
+      case Cons(h,t) => foldLeft(t)(f(acc, h))(f)
+      case _ => acc
+
+    def reverse[A](l: List[A]): List[A] = l match
+      case Cons(h,t) => append(reverse(t), Cons(h, Nil()))
+      case Nil() => Nil()
+
+
+    def foldRight[A,B](l: List[A])(acc: B)(f: (A, B) => B): B =
+      foldLeft(reverse(l))(acc)((a,b) => f(b,a))
+
+
 
   val l = List.Cons(10, List.Cons(20, List.Cons(30, List.Nil())))
   println(List.sum(l)) // 60
--- src/test/scala/u03/ListTest.scala
+++ src/test/scala/u03/ListTest.scala
@@ -3,6 +3,8 @@ package u03
 import org.junit.*
 import org.junit.Assert.*
 import Lists.*
+import u02.Optionals.Option
+import u02.Modules.*
 
 class ListTest:
   import List.*
@@ -33,3 +35,22 @@ class ListTest:
   @Test def testFlatMap() =
     assertEquals(Cons (11, Cons (21, Cons (31, Nil ()))), flatMap(l)(v => Cons (v + 1, Nil ())))
     assertEquals(Cons (11, Cons(12, Cons(21, Cons(22, Cons(31, Cons(32, Nil())))))), flatMap(l) (v => Cons(v + 1, Cons(v + 2, Nil()))))
+
+  @Test def testMax() =
+    //assertEquals(Option.Some(25), max(Cons (10, Cons(25, Cons (20, Nil ())))))
+    assertEquals(Option.Some(25), max(Cons(10,Cons(25, Cons(20, Nil())))))
+
+  @Test def testGetTeacherCourses() =
+    assertEquals(Cons("pps", Nil()), getTeacherCourses(Cons(Person.Student("Anna", 2022), Cons(Person.Teacher("Mirko", "pps"), Nil()))))
+
+  @Test def testFoldLeft() =
+    val lst = Cons(3, Cons (7, Cons (1, Cons (5, Nil ()))))
+    assertEquals(-16, foldLeft(lst)(0)(_-_))
+
+  @Test def testReverse() =
+    val lst = Cons(3, Cons (7, Cons (1, Cons (5, Nil ()))))
+    assertEquals(Cons(5, Cons (1, Cons (7, Cons (3, Nil ())))), reverse(lst))
+
+  @Test def testFoldRight() =
+    val lst = Cons(3, Cons (7, Cons (1, Cons (5, Nil ()))))
+    assertEquals(-8, foldRight(lst)(0)(_-_))
